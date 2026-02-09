be552a66f7369a1933f4c784c0f85b452f9f213e
Daniel <danielmarper2002@gmail.com>
2026-02-09 23:56:21 +0100

    include some more keybinds

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,6 +10,8 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<C-f>", "<C-f>zz")
 vim.keymap.set("n", "<C-b>", "<C-b>zz")
+vim.keymap.set("n", "n", "nzz")
+vim.keymap.set("n", "N", "Nzz")
 
 -- Default keymaps
 vim.keymap.set({ "n", "x" }, "p", "<Plug>(YankyPutAfter)")
