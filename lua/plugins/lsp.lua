8ea506c0bbe00ae6ebc42f3c12e860e242e1b5b8
Daniel <danielmarper2002@gmail.com>
2026-03-06 18:17:21 +0100

    change tree-sitter (lsp not fully working)

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,8 +1,40 @@
 return {
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    "mason-org/mason.nvim",
     "mason-org/mason-lspconfig.nvim",
-    opts = {},
-    dependencies = {
-        { "mason-org/mason.nvim", opts = {} },
-        "neovim/nvim-lspconfig",
-    },
+  },
+  config = function()
+    -- 1. Initialize Mason
+    require("mason").setup()
+
+    -- 2. Optional: Configure custom settings natively BEFORE setting up mason-lspconfig
+    vim.lsp.config("pyright", {
+      settings = {
+        python = {
+          analysis = {
+            typeCheckingMode = "basic",
+          }
+        }
+      }
+    })
+
+    -- 3. Initialize Mason-LSPconfig (automatically enables the servers in Neovim 0.11+)
+    require("mason-lspconfig").setup({
+      ensure_installed = { "lua_ls", "pyright", "ruff" }, 
+    })
+
+    -- 4. Set up basic keybindings when an LSP attaches to a buffer
+    vim.api.nvim_create_autocmd('LspAttach', {
+      desc = 'LSP actions',
+      callback = function(event)
+        local opts = { buffer = event.buf }
+
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+        vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+        vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+      end,
+    })
+  end,
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,22 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  lazy = false,
-  build = ':TSUpdate'
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  opts = {
+    -- Add the languages you program in to this list
+    ensure_installed = { "cpp", "lua", "python"},
+
+    -- Automatically install missing parsers when entering a buffer
+    auto_install = true,
+
+    highlight = {
+      -- This enables the tree-sitter highlighting
+      enable = true, 
+      
+      -- Disables standard vim highlighting to prevent conflicts and lag
+      additional_vim_regex_highlighting = false,
+    },
+  },
+  config = function(_, opts)
+    require("nvim-treesitter.configs").setup(opts)
+  end
 }
